a3ed37ea84775fb4a63aef64565336897305892a
Nao Yonashiro <owan.orisano@gmail.com>
2021-05-02 19:06:15 +0900

    feat: bce

--- sort.go
+++ sort.go
@@ -38,38 +38,40 @@ func encodeFloat64(x float64) uint64 {
 }
 
 func SortInt32(a []int32) {
-	var level0, level1, level2, level3 [257]int
+	var level0, level1, level2, level3 [256]int
 	b := make([]int32, len(a))
 	for _, v := range a {
 		x := encodeInt32(v)
-		level0[((x>>0)&0xff)+1]++
-		level1[((x>>8)&0xff)+1]++
-		level2[((x>>16)&0xff)+1]++
-		level3[((x>>24)&0xff)+1]++
+		level0[uint8((x>>0)&0xff)]++
+		level1[uint8((x>>8)&0xff)]++
+		level2[uint8((x>>16)&0xff)]++
+		level3[uint8((x>>24)&0xff)]++
 	}
-	for i := 1; i < 257; i++ {
-		level0[i] += level0[i-1]
-		level1[i] += level1[i-1]
-		level2[i] += level2[i-1]
-		level3[i] += level3[i-1]
+	var l0, l1, l2, l3 int
+	for i := 0; i < 256; i++ {
+		i := uint8(i)
+		l0, level0[i] = l0+level0[i], l0
+		l1, level1[i] = l1+level1[i], l1
+		l2, level2[i] = l2+level2[i], l2
+		l3, level3[i] = l3+level3[i], l3
 	}
 	for _, v := range a {
-		x := (encodeInt32(v) >> 0) & 0xff
+		x := uint8((encodeInt32(v) >> 0) & 0xff)
 		b[level0[x]] = v
 		level0[x]++
 	}
 	for _, v := range b {
-		x := (encodeInt32(v) >> 8) & 0xff
+		x := uint8((encodeInt32(v) >> 8) & 0xff)
 		a[level1[x]] = v
 		level1[x]++
 	}
 	for _, v := range a {
-		x := (encodeInt32(v) >> 16) & 0xff
+		x := uint8((encodeInt32(v) >> 16) & 0xff)
 		b[level2[x]] = v
 		level2[x]++
 	}
 	for _, v := range b {
-		x := (encodeInt32(v) >> 24) & 0xff
+		x := uint8((encodeInt32(v) >> 24) & 0xff)
 		a[level3[x]] = v
 		level3[x]++
 	}
@@ -80,206 +82,216 @@ func SortUint32(a []uint32) {
 	b := make([]uint32, len(a))
 	for _, v := range a {
 		x := v
-		level0[((x>>0)&0xff)+1]++
-		level1[((x>>8)&0xff)+1]++
-		level2[((x>>16)&0xff)+1]++
-		level3[((x>>24)&0xff)+1]++
+		level0[uint8((x>>0)&0xff)]++
+		level1[uint8((x>>8)&0xff)]++
+		level2[uint8((x>>16)&0xff)]++
+		level3[uint8((x>>24)&0xff)]++
 	}
-	for i := 1; i < 257; i++ {
-		level0[i] += level0[i-1]
-		level1[i] += level1[i-1]
-		level2[i] += level2[i-1]
-		level3[i] += level3[i-1]
+	var l0, l1, l2, l3 int
+	for i := 0; i < 256; i++ {
+		i := uint8(i)
+		l0, level0[i] = l0+level0[i], l0
+		l1, level1[i] = l1+level1[i], l1
+		l2, level2[i] = l2+level2[i], l2
+		l3, level3[i] = l3+level3[i], l3
 	}
 	for _, v := range a {
-		x := (v >> 0) & 0xff
+		x := uint8((v >> 0) & 0xff)
 		b[level0[x]] = v
 		level0[x]++
 	}
 	for _, v := range b {
-		x := (v >> 8) & 0xff
+		x := uint8((v >> 8) & 0xff)
 		a[level1[x]] = v
 		level1[x]++
 	}
 	for _, v := range a {
-		x := (v >> 16) & 0xff
+		x := uint8((v >> 16) & 0xff)
 		b[level2[x]] = v
 		level2[x]++
 	}
 	for _, v := range b {
-		x := (v >> 24) & 0xff
+		x := uint8((v >> 24) & 0xff)
 		a[level3[x]] = v
 		level3[x]++
 	}
 }
 
 func SortInt64(a []int64) {
-	var level0, level1, level2, level3 [257]int
-	var level4, level5, level6, level7 [257]int
+	var level0, level1, level2, level3 [256]int
+	var level4, level5, level6, level7 [256]int
 	b := make([]int64, len(a))
 	for _, v := range a {
 		x := encodeInt64(v)
-		level0[((x>>0)&0xff)+1]++
-		level1[((x>>8)&0xff)+1]++
-		level2[((x>>16)&0xff)+1]++
-		level3[((x>>24)&0xff)+1]++
-		level4[((x>>32)&0xff)+1]++
-		level5[((x>>40)&0xff)+1]++
-		level6[((x>>48)&0xff)+1]++
-		level7[((x>>56)&0xff)+1]++
+		level0[uint8((x>>0)&0xff)]++
+		level1[uint8((x>>8)&0xff)]++
+		level2[uint8((x>>16)&0xff)]++
+		level3[uint8((x>>24)&0xff)]++
+		level4[uint8((x>>32)&0xff)]++
+		level5[uint8((x>>40)&0xff)]++
+		level6[uint8((x>>48)&0xff)]++
+		level7[uint8((x>>56)&0xff)]++
 	}
-	for i := 1; i < 257; i++ {
-		level0[i] += level0[i-1]
-		level1[i] += level1[i-1]
-		level2[i] += level2[i-1]
-		level3[i] += level3[i-1]
-		level4[i] += level4[i-1]
-		level5[i] += level5[i-1]
-		level6[i] += level6[i-1]
-		level7[i] += level7[i-1]
+	var l0, l1, l2, l3 int
+	var l4, l5, l6, l7 int
+	for i := 0; i < 256; i++ {
+		i := uint8(i)
+		l0, level0[i] = l0+level0[i], l0
+		l1, level1[i] = l1+level1[i], l1
+		l2, level2[i] = l2+level2[i], l2
+		l3, level3[i] = l3+level3[i], l3
+		l4, level4[i] = l4+level4[i], l4
+		l5, level5[i] = l5+level5[i], l5
+		l6, level6[i] = l6+level6[i], l6
+		l7, level7[i] = l7+level7[i], l7
 	}
 	for _, v := range a {
-		x := (encodeInt64(v) >> 0) & 0xff
+		x := uint8((encodeInt64(v) >> 0) & 0xff)
 		b[level0[x]] = v
 		level0[x]++
 	}
 	for _, v := range b {
-		x := (encodeInt64(v) >> 8) & 0xff
+		x := uint8((encodeInt64(v) >> 8) & 0xff)
 		a[level1[x]] = v
 		level1[x]++
 	}
 	for _, v := range a {
-		x := (encodeInt64(v) >> 16) & 0xff
+		x := uint8((encodeInt64(v) >> 16) & 0xff)
 		b[level2[x]] = v
 		level2[x]++
 	}
 	for _, v := range b {
-		x := (encodeInt64(v) >> 24) & 0xff
+		x := uint8((encodeInt64(v) >> 24) & 0xff)
 		a[level3[x]] = v
 		level3[x]++
 	}
 	for _, v := range a {
-		x := (encodeInt64(v) >> 32) & 0xff
+		x := uint8((encodeInt64(v) >> 32) & 0xff)
 		b[level4[x]] = v
 		level4[x]++
 	}
 	for _, v := range b {
-		x := (encodeInt64(v) >> 40) & 0xff
+		x := uint8((encodeInt64(v) >> 40) & 0xff)
 		a[level5[x]] = v
 		level5[x]++
 	}
 	for _, v := range a {
-		x := (encodeInt64(v) >> 48) & 0xff
+		x := uint8((encodeInt64(v) >> 48) & 0xff)
 		b[level6[x]] = v
 		level6[x]++
 	}
 	for _, v := range b {
-		x := (encodeInt64(v) >> 56) & 0xff
+		x := uint8((encodeInt64(v) >> 56) & 0xff)
 		a[level7[x]] = v
 		level7[x]++
 	}
 }
 
 func SortUint64(a []uint64) {
-	var level0, level1, level2, level3 [257]int
-	var level4, level5, level6, level7 [257]int
+	var level0, level1, level2, level3 [256]int
+	var level4, level5, level6, level7 [256]int
 	b := make([]uint64, len(a))
 	for _, v := range a {
 		x := v
-		level0[((x>>0)&0xff)+1]++
-		level1[((x>>8)&0xff)+1]++
-		level2[((x>>16)&0xff)+1]++
-		level3[((x>>24)&0xff)+1]++
-		level4[((x>>32)&0xff)+1]++
-		level5[((x>>40)&0xff)+1]++
-		level6[((x>>48)&0xff)+1]++
-		level7[((x>>56)&0xff)+1]++
+		level0[uint8((x>>0)&0xff)]++
+		level1[uint8((x>>8)&0xff)]++
+		level2[uint8((x>>16)&0xff)]++
+		level3[uint8((x>>24)&0xff)]++
+		level4[uint8((x>>32)&0xff)]++
+		level5[uint8((x>>40)&0xff)]++
+		level6[uint8((x>>48)&0xff)]++
+		level7[uint8((x>>56)&0xff)]++
 	}
-	for i := 1; i < 257; i++ {
-		level0[i] += level0[i-1]
-		level1[i] += level1[i-1]
-		level2[i] += level2[i-1]
-		level3[i] += level3[i-1]
-		level4[i] += level4[i-1]
-		level5[i] += level5[i-1]
-		level6[i] += level6[i-1]
-		level7[i] += level7[i-1]
+	var l0, l1, l2, l3 int
+	var l4, l5, l6, l7 int
+	for i := 0; i < 256; i++ {
+		i := uint8(i)
+		l0, level0[i] = l0+level0[i], l0
+		l1, level1[i] = l1+level1[i], l1
+		l2, level2[i] = l2+level2[i], l2
+		l3, level3[i] = l3+level3[i], l3
+		l4, level4[i] = l4+level4[i], l4
+		l5, level5[i] = l5+level5[i], l5
+		l6, level6[i] = l6+level6[i], l6
+		l7, level7[i] = l7+level7[i], l7
 	}
 	for _, v := range a {
-		x := (v >> 0) & 0xff
+		x := uint8((v >> 0) & 0xff)
 		b[level0[x]] = v
 		level0[x]++
 	}
 	for _, v := range b {
-		x := (v >> 8) & 0xff
+		x := uint8((v >> 8) & 0xff)
 		a[level1[x]] = v
 		level1[x]++
 	}
 	for _, v := range a {
-		x := (v >> 16) & 0xff
+		x := uint8((v >> 16) & 0xff)
 		b[level2[x]] = v
 		level2[x]++
 	}
 	for _, v := range b {
-		x := (v >> 24) & 0xff
+		x := uint8((v >> 24) & 0xff)
 		a[level3[x]] = v
 		level3[x]++
 	}
 	for _, v := range a {
-		x := (v >> 32) & 0xff
+		x := uint8((v >> 32) & 0xff)
 		b[level4[x]] = v
 		level4[x]++
 	}
 	for _, v := range b {
-		x := (v >> 40) & 0xff
+		x := uint8((v >> 40) & 0xff)
 		a[level5[x]] = v
 		level5[x]++
 	}
 	for _, v := range a {
-		x := (v >> 48) & 0xff
+		x := uint8((v >> 48) & 0xff)
 		b[level6[x]] = v
 		level6[x]++
 	}
 	for _, v := range b {
-		x := (v >> 56) & 0xff
+		x := uint8((v >> 56) & 0xff)
 		a[level7[x]] = v
 		level7[x]++
 	}
 }
 
 func SortFloat32(a []float32) {
-	var level0, level1, level2, level3 [257]int
+	var level0, level1, level2, level3 [256]int
 	b := make([]float32, len(a))
 	for _, v := range a {
 		x := encodeFloat32(v)
-		level0[((x>>0)&0xff)+1]++
-		level1[((x>>8)&0xff)+1]++
-		level2[((x>>16)&0xff)+1]++
-		level3[((x>>24)&0xff)+1]++
+		level0[uint8((x>>0)&0xff)]++
+		level1[uint8((x>>8)&0xff)]++
+		level2[uint8((x>>16)&0xff)]++
+		level3[uint8((x>>24)&0xff)]++
 	}
-	for i := 1; i < 257; i++ {
-		level0[i] += level0[i-1]
-		level1[i] += level1[i-1]
-		level2[i] += level2[i-1]
-		level3[i] += level3[i-1]
+	var l0, l1, l2, l3 int
+	for i := 0; i < 256; i++ {
+		i := uint8(i)
+		l0, level0[i] = l0+level0[i], l0
+		l1, level1[i] = l1+level1[i], l1
+		l2, level2[i] = l2+level2[i], l2
+		l3, level3[i] = l3+level3[i], l3
 	}
 	for _, v := range a {
-		x := (encodeFloat32(v) >> 0) & 0xff
+		x := uint8((encodeFloat32(v) >> 0) & 0xff)
 		b[level0[x]] = v
 		level0[x]++
 	}
 	for _, v := range b {
-		x := (encodeFloat32(v) >> 8) & 0xff
+		x := uint8((encodeFloat32(v) >> 8) & 0xff)
 		a[level1[x]] = v
 		level1[x]++
 	}
 	for _, v := range a {
-		x := (encodeFloat32(v) >> 16) & 0xff
+		x := uint8((encodeFloat32(v) >> 16) & 0xff)
 		b[level2[x]] = v
 		level2[x]++
 	}
 	for _, v := range b {
-		x := (encodeFloat32(v) >> 24) & 0xff
+		x := uint8((encodeFloat32(v) >> 24) & 0xff)
 		a[level3[x]] = v
 		level3[x]++
 	}
@@ -291,62 +303,65 @@ func SortFloat64(a []float64) {
 	b := make([]float64, len(a))
 	for _, v := range a {
 		x := encodeFloat64(v)
-		level0[((x>>0)&0xff)+1]++
-		level1[((x>>8)&0xff)+1]++
-		level2[((x>>16)&0xff)+1]++
-		level3[((x>>24)&0xff)+1]++
-		level4[((x>>32)&0xff)+1]++
-		level5[((x>>40)&0xff)+1]++
-		level6[((x>>48)&0xff)+1]++
-		level7[((x>>56)&0xff)+1]++
+		level0[uint8((x>>0)&0xff)]++
+		level1[uint8((x>>8)&0xff)]++
+		level2[uint8((x>>16)&0xff)]++
+		level3[uint8((x>>24)&0xff)]++
+		level4[uint8((x>>32)&0xff)]++
+		level5[uint8((x>>40)&0xff)]++
+		level6[uint8((x>>48)&0xff)]++
+		level7[uint8((x>>56)&0xff)]++
 	}
-	for i := 1; i < 257; i++ {
-		level0[i] += level0[i-1]
-		level1[i] += level1[i-1]
-		level2[i] += level2[i-1]
-		level3[i] += level3[i-1]
-		level4[i] += level4[i-1]
-		level5[i] += level5[i-1]
-		level6[i] += level6[i-1]
-		level7[i] += level7[i-1]
+	var l0, l1, l2, l3 int
+	var l4, l5, l6, l7 int
+	for i := 0; i < 256; i++ {
+		i := uint8(i)
+		l0, level0[i] = l0+level0[i], l0
+		l1, level1[i] = l1+level1[i], l1
+		l2, level2[i] = l2+level2[i], l2
+		l3, level3[i] = l3+level3[i], l3
+		l4, level4[i] = l4+level4[i], l4
+		l5, level5[i] = l5+level5[i], l5
+		l6, level6[i] = l6+level6[i], l6
+		l7, level7[i] = l7+level7[i], l7
 	}
 	for _, v := range a {
-		x := (encodeFloat64(v) >> 0) & 0xff
+		x := uint8((encodeFloat64(v) >> 0) & 0xff)
 		b[level0[x]] = v
 		level0[x]++
 	}
 	for _, v := range b {
-		x := (encodeFloat64(v) >> 8) & 0xff
+		x := uint8((encodeFloat64(v) >> 8) & 0xff)
 		a[level1[x]] = v
 		level1[x]++
 	}
 	for _, v := range a {
-		x := (encodeFloat64(v) >> 16) & 0xff
+		x := uint8((encodeFloat64(v) >> 16) & 0xff)
 		b[level2[x]] = v
 		level2[x]++
 	}
 	for _, v := range b {
-		x := (encodeFloat64(v) >> 24) & 0xff
+		x := uint8((encodeFloat64(v) >> 24) & 0xff)
 		a[level3[x]] = v
 		level3[x]++
 	}
 	for _, v := range a {
-		x := (encodeFloat64(v) >> 32) & 0xff
+		x := uint8((encodeFloat64(v) >> 32) & 0xff)
 		b[level4[x]] = v
 		level4[x]++
 	}
 	for _, v := range b {
-		x := (encodeFloat64(v) >> 40) & 0xff
+		x := uint8((encodeFloat64(v) >> 40) & 0xff)
 		a[level5[x]] = v
 		level5[x]++
 	}
 	for _, v := range a {
-		x := (encodeFloat64(v) >> 48) & 0xff
+		x := uint8((encodeFloat64(v) >> 48) & 0xff)
 		b[level6[x]] = v
 		level6[x]++
 	}
 	for _, v := range b {
-		x := (encodeFloat64(v) >> 56) & 0xff
+		x := uint8((encodeFloat64(v) >> 56) & 0xff)
 		a[level7[x]] = v
 		level7[x]++
 	}
